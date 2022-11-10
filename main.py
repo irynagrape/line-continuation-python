1d43320642edb54fc873fca3a94029c7e27e5b4c
irynagrape <109566237+irynagrape@users.noreply.github.com>
2022-11-09 21:35:59 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+print("I know Python and its very easy for everyone")
+
+print("I know Python and \
+ its very easy for everyone")
+
+#In Python, a backslash ( \ ) is a continuation character.
+
+print("I know Python \ and its very easy for everyone")
